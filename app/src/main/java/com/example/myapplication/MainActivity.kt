50c73e8aab44be1c9b914ac6c1209a1c14368bca
Mauricio Garcia <mauricaz@outlook.com>
2023-02-03 18:21:34 -0600

    Clase Viernes

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,22 +2,65 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
+import com.bumptech.glide.Glide
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
 
     private var x : Int = 0
 
+    /*
+        Vinculación de vistra tradicional con findViewById
+
+    private lateinit var myImage: ImageView
+    private lateinit var myEditText: TextInputLayout
+    private lateinit var myButton: Button
+
+    */
+
+    // Vinculación de vista usando viewBinding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        println(x)
-        x = 10
-        println(x)
-        x += 9
-        println(x)
+        /*
+        myImage = findViewById(R.id.imagen)
+        myEditText = findViewById(R.id.textField)
+        myButton = findViewById(R.id.button)
+        */
+
+        binding.button.setOnClickListener {  view ->
+            val textoIngresado = binding.textField.editText?.text.toString()
+            if (textoIngresado.isBlank()) {
+                Toast.makeText(this, "Por favor ingresa tu nombre", Toast.LENGTH_LONG).show()
+            } else {
+                val snackbar =
+                    Snackbar.make(binding.root, getString(R.string.messageName, textoIngresado), Snackbar.LENGTH_LONG)
+                        .setBackgroundTint(getColor(R.color.terciario))
+                        .setAction("Ok") { }
+                        .show()
+            }
+        }
+
+        val items = listOf("México", "EUA", "España","Colombia","Uruguay","Argentina","Brasil","Venezuela")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_selectable_list_item , items)
+        (binding.menuPaises.editText as? AutoCompleteTextView)?.setAdapter(adapter)
+
+        loadImage()
     }
 
+    private fun loadImage() {
+        Glide.with(this)
+            .load("https://imagenpng.com/wp-content/uploads/2015/09/Paper_mario.png")
+            .centerCrop()
+            .into(binding.imagen)
+    }
 
 
 }
